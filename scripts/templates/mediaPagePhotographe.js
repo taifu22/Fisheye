38fc26499b6a0f3d7769546e1eb6c2c3792fa076
taoufik chahouat <taoufik.chahouat@gmail.com>
2022-04-16 17:04:10 +0200

    page photographe, mise en place des likes

--- scripts/templates/mediaPagePhotographe.js
+++ scripts/templates/mediaPagePhotographe.js
@@ -41,48 +41,60 @@ class MediaPagePhotographe {
     //on map notre array avec les videos et on créé pour chaque élément une balise video, pour l'afficher dans le navigateur
     this.arrayVideos.map((el) => {
       const video = document.createElement("div");
-      video.innerHTML = `<video class="images-photographers" controls> 
+      video.innerHTML = `<video style="cursor:pointer;" class="images-photographers lightbox-activate" controls> 
                    <source src="/assets/images/${this.array[0].name}/${el}" type="video/mp4">
                    </video>
                    <div class="p-heart">
                    <p>${this.arrayTitles[index]}</p>
-                   <div id="${this.arrayIdMedias[id]}" class="heart"><p>${this.arrayLikes[like]}</p><i class="fa-solid fa-heart"></i></div>
+                   <div id="${this.arrayIdMedias[id]}" class="heart"><p id="${this.arrayIdMedias[id]}" class="p-like">${this.arrayLikes[like]}</p><i class="fa-solid fa-heart"></i></div>
                    </div>`;
-      video.setAttribute("class", "lightbox-activate card-photo");
-      video.style.cursor = "pointer";
+      video.setAttribute('class', 'card-photo')             
       this.photosMedias.appendChild(video);
     });
 
-    //fonction pour ajouter et enlever le like pour chaque media
-    //let checkLike = true;
-    let numero = 0
+    //calcule totale des likes dans le tableau arrayLikes (result contiendra notre totale)
     let arrayLikes1 = this.arrayLikes
+    let result = 0;
+    for (let i = 0; i < arrayLikes1.length; i++) {
+      let numero = arrayLikes1[i];
+      result += numero 
+    }
+
+    //affichage des likes totales des medias dans chaque page photographe
+    const likeTotale = document.querySelector('.like-fixed');
+    likeTotale.innerHTML = `<div><p class="plike-totale">${result}</p><i class="fa-solid fa-heart"></i></div>
+                             <p class="plike-price">${this.array[0].price} / jour</p>`;
+
+    //fonction pour ajouter et enlever le like pour chaque media
+    let numero = 0;
+    let totale = 0;
     let photo1Index = null;
     function clickLike(e) {
       let plike = document.querySelectorAll(".p-like");
+      let plikeTotaleLikes = document.querySelector('.plike-totale');
       let currentImage = e.currentTarget.cloneNode(true);
       let currentIndex = currentImage.querySelector('p').textContent;
-      //numero = parseInt(document.querySelector('.p-like').textContent);
       photo1Index = arrayLikes1.indexOf(parseInt(currentIndex))
-      console.log(photo1Index);
+      
       plike.forEach(btn => {
         let numeroid = btn.id
         if (numeroid === currentImage.id && btn.textContent == arrayLikes1[photo1Index]) {
           numero = parseInt(btn.textContent);
-          console.log("ca marche en plus");
+          totale = parseInt(document.querySelector('.plike-totale').textContent)
           btn.innerHTML = numero + 1;
+          plikeTotaleLikes.innerHTML = totale + 1; 
         } else if (numeroid === currentImage.id && btn.textContent != arrayLikes1[photo1Index]) {
           numero = parseInt(btn.textContent);
-          console.log("ca marche en moins");
+          totale = parseInt(document.querySelector('.plike-totale').textContent)
           btn.innerHTML = numero - 1;
-          //checkLike = true;
+          plikeTotaleLikes.innerHTML = totale - 1; 
         } 
       })   
     }
 
     //evenement pour déclancher la fonction créé en haut concernant les likes de chaque media
     const eventLikes = document.querySelectorAll(".heart");
-    //eventLikes.addEventListener("click", clickLike)
     eventLikes.forEach((btn) => btn.addEventListener("click", clickLike));
+    
   }
 }
